0bb4cef58a0d4e2a8f47b8286981b96478d7858a
DenskNOW <denis4apro@gmail.com>
2025-07-28 22:28:11 +0300

    Добавлен запуск main()

--- main.py
+++ main.py
@@ -41,4 +41,17 @@ async def scheduler_loop(app):
     while True:
         schedule.run_pending()
         await asyncio.sleep(1)
+async def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button_callback))
+
+    # Параллельный запуск Telegram и планировщика
+    await asyncio.gather(
+        app.run_polling(),
+        scheduler_loop(app)
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
